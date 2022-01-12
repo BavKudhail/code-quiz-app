f41ecc71a3c13be1c3fcf1d211f26d99b76feaf0
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-01-12 15:49:03 +0000

    Created further functions that together act as the foundation of the quiz app

--- assets/script.js
+++ assets/script.js
@@ -4,10 +4,7 @@ var scoreEl = document.getElementById("score-text");
 var questionContainerEl = document.getElementById("question-container");
 var questionEl = document.getElementById("question");
 var optionEl = document.getElementById("option-buttons")
-
-var currentQuestionIndex 
-
-startBtn.addEventListener("click", startGame)
+var headerEl = document.getElementById("header")
 
 var questions = [
     {
@@ -36,41 +33,141 @@ var questions = [
             { option: "function => (myFunction)", correct: false},
             { option: "function :: myFunction()", correct: false},
         ]
-    }
+    },
+     {
+        question: "How do you call a function named 'myFunction'?",
+        options: [
+            { option: "call myFunction()", correct: false},
+            { option: "myFunction()", correct: true},
+            { option: "call function myFunction()", correct: false},
+            { option: "function => function()", correct: false},
+        ]
+    },
+     {
+        question: "How to write an IF statement in JavaScript?",
+        options: [ 
+            { option: "if i = 5 then", correct: false },
+            { option: "if (i == 5)", correct: false },
+            { option: "if i = 5", correct: true },
+            { option: "if i == 5 then", correct: false },
+        ]
+    },
+    {
+        question: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
+        options: [
+            { option: "if i <> 5", correct: false},
+            { option: "if (i !-5)", correct: false},
+            { option: "if i =! 5 then", correct: false},
+            { option: "if (i <> 5)", correct: true}
+        ]
+    },
+    {
+        question: "How does a FOR loop start?",
+        options: [
+            { option: "for i = 1 to 5", correct: true},
+            { option: "for (i = 0; i < = 5)", correct: false},
+            { option: "for (i <= 5; i++)", correct: false},
+            { option: "for (i = 0; i <= 5; i++)", correct: true},
+        ]
+    },
+     {
+        question: "How can you add a comment in a JavaScript?",
+        options: [
+            { option: "'This is a comment'", correct: false},
+            { option: "<!-- This is a comment -->", correct: true},
+            { option: "<!== This is a comment ==>", correct: false},
+            { option: "//This is a comment", correct: true},
+        ]
+    },
 ]
+
+var maximumQuestions = questions.length
+var currentQuestionIndex 
+
+startBtn.addEventListener("click", startGame)
+
 
 function startGame(){
 
     startBtn.classList.add("hide");
     currentQuestionIndex = 0;
-    questionContainerEl.classList.remove("hide");
 
-    setNextQuestion()
+    questionContainerEl.classList.remove("hide");
+    headerEl.classList.remove("hide")
+
+    getNextQuestion()
 
 }
 
-function setNextQuestion(){
-
+// Get the next question
+function getNextQuestion(){
+    resetDisplay()
     showQuestion(questions[currentQuestionIndex])
 
 }
 
-function showQuestion(question){
-    questionEl.innerText = question.question
-    question.options.forEach(Element => {
-
+// Show questions on display
+function showQuestion(x){
+    questionEl.innerText = x.question
+    x.options.forEach(Element => {
+        
         var button = document.createElement('button')
-        button.innerText = Element.options
+        button.innerText = Element.option
 
         if (Element.correct) {
             button.dataset.correct = Element.correct
         }
 
-        button.addEventListener('click', selectAnswer)
         questionEl.appendChild(button)
+        button.addEventListener('click', selectAnswer)
     })
-    
 }
+
+
+// Remove previous buttons from display
+function resetDisplay(){
+    while (optionEl.firstChild) {
+        optionEl.removeChild(optionEl.firstChild)
+    }
+}
+
+
+// When user selects a button
 function selectAnswer(e){
+    var selectedButton = e.target
+    var correct = selectedButton.dataset.correct
+
+    setClass(selectedButton, correct)
+
+    Array.from(optionEl.children).forEach(button => {
+        setClass(button, button.dataset.correct)
+    })
+
+    // If there are more questions remaining, getNextQuestion
+    if (maximumQuestions > currentQuestionIndex + 1) {
+        currentQuestionIndex++
+        setInterval (getNextQuestion, 1000)
+    // Else display options for restart
+    } else {
+        startBtn.innerText = "Restart"
+        startBtn.classList.remove('hide')
+    }
+}
+
+// If correct, add correct class. Else, add incorrect class.
+function setClass(element, correct){
+    clearClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('incorrect')
+    }
+}
+
+// Remove classes
+function clearClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('incorrect')
 
 }
+
