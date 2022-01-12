456dfcb131972b47db3918a5202ab14ac507e678
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-01-12 12:01:38 +0000

    created display hide class

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+var startBtn = document.getElementById("start-btn");
+var timerEl = document.getElementById("timer-text");
+var scoreEl = document.getElementById("score-text");
+
+startBtn.addEventListener("click", startGame)
+
+
+
+function startGame(){
+    console.log("game has started");
+    
+
+}
+
+// start game 
+
+
+
+// setNextQuestion
+
+
+
+// selectAnswer
